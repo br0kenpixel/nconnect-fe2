892b0d82637ca35154e788b69ad911ff16218aa6
Fábián Varga <23280129+br0kenpixel@users.noreply.github.com>
2024-06-02 09:58:54 +0200

    fix padding

--- components/TopInfoBar.vue
+++ components/TopInfoBar.vue
@@ -1,8 +1,8 @@
 <script lang="ts" setup></script>
 
 <template>
-    <div class="container-fluid text-center">
-        <div class="row">
+    <div class="container-fluid text-center root">
+        <div class="row" id="topBarRoot">
             <div class="col-sm">📍 Študentské centrum UKF, Dražovská 2, Nitra</div>
             <div class="col-sm">📞 +421 902 170 744</div>
             <div class="col-sm">✉️ info@nconnect.sk</div>
@@ -11,10 +11,13 @@
 </template>
 
 <style scoped>
+#topBarRoot {
+    padding: 10px !important;
+}
+
 div {
     background-color: rgb(6, 57, 66);
     color: white;
-    padding: 3px;
     font-size: 14px;
     font-family: 'Lato', sans-serif;
 }
