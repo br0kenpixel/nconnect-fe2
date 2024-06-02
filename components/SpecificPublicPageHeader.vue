cb8cb0e08b3ef2f01785c4c88b198386d95afa8d
Fábián Varga <23280129+br0kenpixel@users.noreply.github.com>
2024-06-02 22:37:28 +0200

    add public page header

---
+++ components/SpecificPublicPageHeader.vue
@@ -0,0 +1,25 @@
+<script lang="ts" setup></script>
+
+<template>
+    <div>
+        <div class="card text-bg-dark">
+            <NuxtImg src="/img/carousel/nconnect.jpg" class="card-img" alt="Nconnect" />
+            <div class="container card-img-overlay d-flex flex-column">
+                <p class="card-text mt-auto">Domov / {{ $route.name }}</p>
+                <h1 class="card-title">{{ $route.name }}</h1>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+h1,
+p {
+    text-transform: uppercase;
+    font-weight: bold;
+}
+
+img {
+    filter: brightness(50%);
+}
+</style>
