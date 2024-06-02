bddc61b30c7af664d343ee460f35dc5fc7458a5b
Fábián Varga <23280129+br0kenpixel@users.noreply.github.com>
2024-06-02 11:35:52 +0200

    fix about area

--- components/AboutArea.vue
+++ components/AboutArea.vue
@@ -1,32 +1,36 @@
 <script lang="ts" setup></script>
 
 <template>
-    <div class="container text-center" id="about-area">
-        <h3>PÁR SLOV O KONFERENCII NCONNECT</h3>
-        <hr>
-        <p>Po mnohých rokoch premýšľania a plánovania sme vytvorili nConnect, jedinečnú udalosť v Nitre, ktorá spája
-            študentov IT a popredné firmy z tohto dynamického odvetvia. Konferencia nConnect nadväzuje na dlhoročnú
-            tradíciu formátu "IT v praxi" Fakulty prírodných vied a informatiky UKF v Nitre. Táto iniciatíva je mostom
-            medzi novou generáciou talentov a skúsenými profesionálmi, ktorý poskytuje fórum pre vzájomnú výmenu
-            myšlienok a inšpirácií. Naše poslanie bolo jasné: vyplniť medzeru v regionálnej komunikácii a spolupráci v
-            IT a nConnect je hrdým výsledkom tejto vízie.</p>
+    <div id="about-area-root">
+        <div class="container text-center" id="about-area">
+            <h3>PÁR SLOV O KONFERENCII NCONNECT</h3>
+            <hr>
+            <p>Po mnohých rokoch premýšľania a plánovania sme vytvorili nConnect, jedinečnú udalosť v Nitre, ktorá spája
+                študentov IT a popredné firmy z tohto dynamického odvetvia. Konferencia nConnect nadväzuje na dlhoročnú
+                tradíciu formátu "IT v praxi" Fakulty prírodných vied a informatiky UKF v Nitre. Táto iniciatíva je
+                mostom
+                medzi novou generáciou talentov a skúsenými profesionálmi, ktorý poskytuje fórum pre vzájomnú výmenu
+                myšlienok a inšpirácií. Naše poslanie bolo jasné: vyplniť medzeru v regionálnej komunikácii a spolupráci
+                v
+                IT a nConnect je hrdým výsledkom tejto vízie.</p>
 
-        <div class="card-group">
-            <div class="card">
-                <div class="card-body">
-                    <h5 class="card-title">KTO SME?</h5>
-                    <hr>
-                    <p class="card-text custom-card-text">Sme skupina nadšencov pre IT z akademického a súkromného
-                        sektora.</p>
+            <div class="card-group">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">KTO SME?</h5>
+                        <hr>
+                        <p class="card-text custom-card-text">Sme skupina nadšencov pre IT z akademického a súkromného
+                            sektora.</p>
+                    </div>
                 </div>
-            </div>
-            <div class="card">
-                <div class="card-body">
-                    <h5 class="card-title">ČO CHCEME DOSIAHNUŤ?</h5>
-                    <hr>
-                    <p class="card-text custom-card-text">Vytvoriť udalosť, ktorá bude na pravidelnej báze spájať IT
-                        komunitu v
-                        Nitre.</p>
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">ČO CHCEME DOSIAHNUŤ?</h5>
+                        <hr>
+                        <p class="card-text custom-card-text">Vytvoriť udalosť, ktorá bude na pravidelnej báze spájať IT
+                            komunitu v
+                            Nitre.</p>
+                    </div>
                 </div>
             </div>
         </div>
@@ -41,9 +45,13 @@ p {
 }
 
 #about-area {
-    margin-top: 50px;
-    margin-bottom: 50px;
+    padding-top: 50px;
+    padding-bottom: 50px;
     width: 85%;
+}
+
+#about-area-root {
+    background: #f0f3f5;
 }
 
 p {
