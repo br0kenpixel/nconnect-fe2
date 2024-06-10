6232a3701f73e14857505c9da758c41245c07db8
Fábián Varga <23280129+br0kenpixel@users.noreply.github.com>
2024-06-10 19:33:53 +0200

    change types path

--- frontend/types/public.ts
+++ frontend/types/public.ts
@@ -1,7 +1,14 @@
 export type Speaker = {
+    id: number;
     name: string;
     image: string;
     company: string;
     description: string;
     headliner: boolean;
+};
+
+export type Sponsor = {
+    id: number;
+    name: string;
+    image: string;
 };
