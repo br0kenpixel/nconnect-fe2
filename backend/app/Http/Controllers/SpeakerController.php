da3eb9a704cf359e915b45f0e0b1adb520780f53
Fábián Varga <23280129+br0kenpixel@users.noreply.github.com>
2024-06-11 19:56:35 +0200

    add speaker crud

--- backend/app/Http/Controllers/SpeakerController.php
+++ backend/app/Http/Controllers/SpeakerController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Models\Speaker;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Validator;
 
 class SpeakerController extends Controller
 {
@@ -12,5 +15,55 @@ class SpeakerController extends Controller
         $speakers = Speaker::all(["id", "name", "company", "description", "image", "headliner"]);
 
         return response()->json($speakers);
+    }
+
+    public function create(Request $request): Response
+    {
+        $validated = Validator::make($request->all(), [
+            "name" => "required|max:64",
+            "company" => "required|max:12",
+            "description" => "required",
+            "image" => "required",
+            "headliner" => "required|boolean"
+        ], $request->all());
+
+        if ($validated->fails()) {
+            return response(status: 400);
+        }
+
+        Speaker::create(request()->all());
+
+        return response(status: 201);
+    }
+
+    public function delete(int $id): Response
+    {
+        Speaker::find($id)->deleteOrFail();
+        return response(status: 204);
+    }
+
+    public function update(int $id, Request $request): Response
+    {
+        $validated = Validator::make($request->all(), [
+            "name" => "required|max:64",
+            "company" => "required|max:12",
+            "description" => "required",
+            "image" => "nullable",
+            "headliner" => "required|boolean"
+        ], $request->all());
+
+        if ($validated->fails()) {
+            return response(status: 400);
+        }
+
+        $keys = $request->all();
+
+        if ($keys["image"] === null) {
+            unset($keys["image"]);
+        }
+
+        Speaker::find($id)->updateOrFail($keys);
+
+        return response(status: 204);
     }
 }
